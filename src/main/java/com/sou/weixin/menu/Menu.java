de7be111ee1ac4344c6930e7bdb8e44773f1667e
shiming25 <1192523554@qq.com>
2016-06-06 20:30:08 +0800

    add

---
+++ src/main/java/com/sou/weixin/menu/Menu.java
@@ -0,0 +1,19 @@
+package com.sou.weixin.menu;
+
+/**
+ * 菜单
+ * 
+ * @author 
+ * @date 2013-10-15
+ */
+public class Menu {
+	private Button[] button;
+
+	public Button[] getButton() {
+		return button;
+	}
+
+	public void setButton(Button[] button) {
+		this.button = button;
+	}
+}
