7fe8687e8a6fc5115991f9a3f381e429843003ab
shiming <437852528@qq.com>
2016-06-22 09:17:01 +0800

    no message

--- src/main/java/com/bijia/weixin/service/handle/LocationMessageHandle.java
+++ src/main/java/com/bijia/weixin/service/handle/LocationMessageHandle.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 import com.alibaba.fastjson.JSONObject;
 import com.github.sd4324530.fastweixin.handle.MessageHandle;
 import com.github.sd4324530.fastweixin.message.BaseMsg;
-import com.github.sd4324530.fastweixin.message.TextMsg;
+import com.github.sd4324530.fastweixin.message.MusicMsg;
 import com.github.sd4324530.fastweixin.message.req.BaseReqMsg;
 import com.github.sd4324530.fastweixin.message.req.ReqType;
 
@@ -28,7 +28,17 @@ public class LocationMessageHandle<M extends BaseReqMsg> implements MessageHandl
 
     @Override
     public BaseMsg handle(M message) {
-        return new TextMsg("handle回复地理位置消息，您好!");
+        MusicMsg musicMsg = new MusicMsg("");
+        if(message !=null) {
+
+            musicMsg.setHqMusicUrl("http://so1.111ttt.com:8282/2016/5/06/22/199220610338.mp3?tflag=1466557886&pin=bbace1fd87ad857652bc0013db75f8e4&ip=180.100.223.13#.mp3");
+            musicMsg.setMusicUrl("http://so1.111ttt.com:8282/2016/5/06/22/199220610338.mp3?tflag=1466557886&pin=bbace1fd87ad857652bc0013db75f8e4&ip=180.100.223.13#.mp3");
+            musicMsg.setThumbMediaId("oAzOSUUKtmhULIVR_jXBVrQEWyafe2XGEuqGw9cChC8jjHND1eGR9Y2a2wYa8ry8");
+            musicMsg.setTitle("testTitle");
+            musicMsg.setDescription("testDesc");
+        }
+        
+        return musicMsg;
     }
 
     @Override
